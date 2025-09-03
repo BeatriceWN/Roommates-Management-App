db17fc0e5146814b8ef84216e062eab8d0f448bc
Praxcedes <praxcedeskabeya@gmail.com>
2025-09-03 16:26:43 +0300

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,3 +2,5 @@ export default function Header() {
   return (
       <header>
       <h1>Roommate Manager</h1>
+       <p className="description">
+        Simplify shared living with chore tracking, bill splitting, and more
