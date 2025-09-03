ac367a4e43505d7c5d0f5736f96a52a30e3a15be
LabanMugutu <labanmugutu@gmail.com>
2025-09-03 17:06:09 +0300

    Adds calendar function

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,1 +1,11 @@
 import { useEffect, useState } from "react";
+function Calendar() {
+  const [events, setEvents] = useState([]);
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:4000/events")
+      .then(res => res.json())
+      .then(data => setEvents(data));
+  }, []);
