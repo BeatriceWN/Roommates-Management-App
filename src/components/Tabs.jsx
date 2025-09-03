2664f852b381f220b4223d5d7c0e852b88c4e4ce
Praxcedes <praxcedeskabeya@gmail.com>
2025-09-03 16:29:11 +0300

    Tabs

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,2 +1,10 @@
 export default function Tabs({ activeTab, setActiveTab }) {
   const tabs = [
+
+    
+ { name: "dashboard", icon: "fa-home", label: "Dashboard" },
+    { name: "chores", icon: "fa-tasks", label: "Chores" },
+    { name: "bills", icon: "fa-money-bill", label: "Bills" },
+    { name: "calendar", icon: "fa-calendar", label: "Calendar" },
+    { name: "roommates", icon: "fa-users", label: "Roommates" },
+  ];
