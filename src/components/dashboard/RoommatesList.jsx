8e48b804d846120e40076575207f378e9481aba1
Beatrice Wambui <bettyneema2@gmail.com>
2025-09-03 17:11:02 +0300

    adds use effect

--- src/components/dashboard/RoommatesList.jsx
+++ src/components/dashboard/RoommatesList.jsx
@@ -6,4 +6,22 @@ function Roommates() {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
 
+  useEffect(() => {
+    fetch("http://localhost:3000/roommates")
+      .then(res => res.json())
+      .then(data => setRoommates(data));
+  }, []);
+
+  const addRoommate = (e) => {
+    e.preventDefault();
+    const newRoommate = { name, email, phone };
+
+    fetch("http://localhost:3000/roommates", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newRoommate)
+    })
+      .then(res => res.json())
+      .then(data => setRoommates([...roommates, data]));
+
   
