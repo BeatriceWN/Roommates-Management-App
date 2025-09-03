20d530766cc76311a6a897779b8be57cba8c55e3
Praxcedes <praxcedeskabeya@gmail.com>
2025-09-03 16:19:41 +0300

    Bills

--- src/components/Bills.jsx
+++ src/components/Bills.jsx
@@ -1,2 +1,8 @@
 import { useState } from "react";
 import { useData } from "../context/DataContext";
+
+export default function Bills() {
+  const { bills, addBill, markBillPaid } = useData();
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [dueDate, setDueDate] = useState("");
